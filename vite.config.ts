be9017c200d7677c5a75c2676f3d42467bbc9be4
John Petersen <johh.b.petersen@gmail.com>
2025-09-05 01:14:37 +0000

    getting some help

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
+        secure: false,
+        ws: true,
+      },
+    },
   },
   test: {
     environment: 'jsdom',
